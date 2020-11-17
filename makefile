7247f01d89bc9d4a697d45c51dd3a76c02ba4d53
rivs <nvawter@6river.com>
2020-11-17 17:50:45 -0500

    scsv.c is super simple csv reader for verifying kompressor/dekompressor

--- makefile
+++ makefile
@@ -2,6 +2,7 @@
 all:
 	gcc -g kompress.c kcsv.c -lm -o kompress
 	gcc -g unkompress.c kcsv.c -o unkompress
+	gcc -g scsv.c -o scsv
 
 
 
---
+++ scsv.c
@@ -0,0 +1,38 @@
+/*
+simple csv reader for test purposes, not mission-hardeened
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN ( 1000 )
+
+int main( int argc, char *argv[] )
+{
+  FILE *fi = fopen( argv[ 1 ], "r" );
+  char buf[ MAX_LEN ];
+  char *saveptr;
+  char *token;
+  
+  if( NULL == fi ){ printf("can't open %s\n", argv[ 1 ] ); return( -1 ); }
+
+  while( 1 )
+  {
+    fgets( buf, MAX_LEN-1, fi );
+    if( feof( fi ) ){ break; }
+    printf("%s", buf );
+
+    token = strtok_r( buf, ",", &saveptr );
+    while( NULL != token )
+    {
+      printf("%s\n", token);
+      token = strtok_r( NULL, ",", &saveptr );
+    }
+
+    
+  }
+   
+
+
+  return 0;
+}
